5bcab4d0f1bc930de54d2a94196c3aa47728296d
OliverMartinCCCP <oliver.cccp@gmail.com>
2018-12-11 13:14:30 +0100

    Reformatting to string to be able to use it as command.

--- config/generic_task.py
+++ config/generic_task.py
@@ -2,12 +2,4 @@ import os
 
 
 GENERIC_TASK_LOG_LEVEL = 0
-GENERIC_TASK_CONFIG = {
-#    'PROXY_FILE': os.path.join(os.path.dirname(__file__), 'proxy_settings'),
-    'LOG_LEVEL_MAPPING': {
-        0: 40,  # logging.ERROR
-        1: 30,  # logging.WARNING
-        2: 20,  # logging.INFO
-        3: 10,  # logging.DEBUG
-    },
-}
+GENERIC_TASK_CONFIG = "{'LOG_LEVEL_MAPPING': {0: 40, 1: 30, 2: 20, 3: 10}}"
